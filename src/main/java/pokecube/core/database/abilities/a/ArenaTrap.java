bf13a58d78827156ecad799f6ef20d3b3b9d02f6
Thutmose <elpatricimo@gmail.com>
2017-07-15 22:21:27 -0400

    adds in the basic pokecube mobs stuff.

---
+++ src/main/java/pokecube/core/database/abilities/a/ArenaTrap.java
@@ -0,0 +1,8 @@
+package pokecube.core.database.abilities.a;
+
+import pokecube.core.database.abilities.Ability;
+
+public class ArenaTrap extends Ability
+{
+    //TODO make this interupt return events and target change events.
+}
