e33c988df4a1787c8dba13ff4a97987e2f0d2664
Thutmose <elpatricimo@gmail.com>
2017-10-22 14:13:02 -0400

    cleanup some formatting

--- src/main/java/pokecube/mobs/Reference.java
+++ src/main/java/pokecube/mobs/Reference.java
@@ -2,7 +2,7 @@ package pokecube.mobs;
 
 public class Reference
 {
-    public static final String VERSION    = "@VERSION@";
+    public final static String VERSION    = "@VERSION@";
     public final static String MINVERSION = "@MINVERSION";
     public final static String MCVERSIONS = "@MCVERSION";
 }
