1a33d1471a7942ff5b20b6035d1cd958fa2147b3
Thutmose <elpatricimo@gmail.com>
2017-12-12 22:28:11 -0500

    fix the mess up from earlier.

--- src/main/java/pokecube/mobs/Reference.java
+++ src/main/java/pokecube/mobs/Reference.java
@@ -1,9 +1,8 @@
-
 package pokecube.mobs;
 
 public class Reference
 {
-    public static final String VERSION    = "@VERSION@";
+    public final static String VERSION    = "@VERSION@";
     public final static String MINVERSION = "@MINVERSION";
     public final static String MCVERSIONS = "@MCVERSION";
 }
